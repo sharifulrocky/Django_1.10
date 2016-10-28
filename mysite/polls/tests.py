84925ee89a308d9465acea9ea358dc5db376b236
shariful <rocky.cse09@gmail.com>
2016-10-28 22:06:25 +0600

    Some test case added in polls app and one is fixed

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.utils import timezone
+import datetime
+from .models import Question
 
-# Create your tests here.
+
+class QuestionMethodTests(TestCase):
+    def tests_was_published_recently_with_future_question(self):
+        """
+            was_published_recently() should return False for questions whose
+            pub_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+            was_published_recently() should return False for questions whose
+            pub_date is older than 1 day.
+        """
+        now = timezone.now()
+        time = now - datetime.timedelta(days=30)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """
+            was_published_recently() should return True for questions whose
+            pub_date is within the last day.
+        """
+        now = timezone.now()
+        time = now - datetime.timedelta(days=1)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
